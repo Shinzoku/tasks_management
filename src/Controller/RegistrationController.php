040e25c40a1b1e28f43d793b255a1a0d921c8d67
Shinzoku <shinzoku62800@gmail.com>
2024-08-07 16:27:18 +0200

    Language change

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -46,7 +46,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('nicolas.bernon62@gmail.com', 'Chef d\'équipe'))
+                    ->from(new Address('nicolas.bernon62@gmail.com', 'Foreman'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')

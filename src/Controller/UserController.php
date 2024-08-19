408356bdd479caeea9dfb25a7cfc8f693ee96006
Shinzoku <shinzoku62800@gmail.com>
2024-08-19 19:50:15 +0200

    user list, show and edit styling and functionnality

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/user')]
@@ -38,7 +39,7 @@ class UserController extends AbstractController
 
         return $this->render('user/new.html.twig', [
             'user' => $user,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
@@ -51,20 +52,32 @@ class UserController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $form->get('password')->getData();
+            
+            if ($password) {
+                // Si l'utilisateur a entré un nouveau mot de passe, on le hash et on l'assigne à l'utilisateur
+                $hashedPassword = $passwordHasher->hashPassword($user, $password);
+                $user->setPassword($hashedPassword);
+            }
+
+            $entityManager->persist($user);
             $entityManager->flush();
+
+            $this->addFlash('success', 'Profil update with success.');
 
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('user/edit.html.twig', [
             'user' => $user,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,26 +6,91 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('isVerified')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new Assert\Email([
+                        'message' => 'Please enter a valid email',
+                    ]),
+                ],
+                'label_attr' => [
+                    'class' => 'inputEmail',
+                    'for' => 'email',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                    'id' => 'email',
+                ],
             ])
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'User' => 'ROLE_USER',
+                ],
+                'label_attr' => [
+                        'class' => 'form-check-label',
+                        'for' => 'checkbox'
+                    ],
+                'attr' => [
+                    'class' => 'form-check-input',
+                    'id' => 'checkbox'
+                ],
+                'required' => false,
+                'expanded' => true,  // Pour afficher comme une liste de cases à cocher
+                'multiple' => true,  // Pour permettre la sélection multiple
             ])
-            ->add('deletedAt', null, [
-                'widget' => 'single_text',
+            ->add('password', PasswordType::class, [
+                'required' => false,  // Le mot de passe est facultatif
+                'mapped' => false,    // Ne lie pas ce champ directement à l'entité User
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'form-control',
+                    'id' => 'password'
+                ],
+                'label_attr' => [
+                    'class' => 'inputPassword',
+                    'for' => 'password',
+                ],
+                'constraints' => [
+                    new Assert\Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        // 'max' => 4096,
+                        'max' => 15,
+                        'maxMessage' => 'Your password cannot be longer than {{ limit }} characters',
+                    ]),
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label_attr' => [
+                    'class' => 'form-label',
+                    'for' => 'firstname',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                    'id' => 'firstname',
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label_attr' => [
+                    'class' => 'form-label',
+                    'for' => 'lastname',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                    'id' => 'lastname',
+                ],
             ])
         ;
     }
